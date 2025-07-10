a7193f9bd2e52f69e0fdb752ef2036404cabdbfa
Maryna Kravets <gum225@gmail.com>
2025-07-10 14:54:59 +0200

    [feat]: added images for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,75 @@
 import useWindowSize from './hooks/useWindowSize'
-import useIsLandscape from './hooks/useIsLandscape'
 
 import Book from './Book'
 import TwinkleOverlay from './TwinkleOverlay'
-import RotateDeviceOverlay from './RotateDevice'
-
-import './App.css'
 import CloudsBackground from './CloudsBackground'
 
+import './App.css'
+import { useState, useRef, useEffect } from 'react'
+
 function App() {
-  const isLandscape = useIsLandscape()
+  const [hasInteracted, setHasInteracted] = useState(false)
   const { width, height, isMobile } = useWindowSize()
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const TOTAL_PAGES = 25
+
+  const getImagePath = (index: number) =>
+    `${import.meta.env.BASE_URL}assets/mobile/mob${index}.webp`
+
+  const pages = Array.from({ length: TOTAL_PAGES }, (_, i) => getImagePath(i))
+
+  useEffect(() => {
+    const handleFirstInteraction = () => {
+      if (!hasInteracted && audioRef.current) {
+        setHasInteracted(true)
+        audioRef.current.play().catch((err) => {
+          console.error('Autoplay failed:', err)
+        })
+      }
+
+      window.removeEventListener('click', handleFirstInteraction)
+      window.removeEventListener('keydown', handleFirstInteraction)
+    }
+
+    window.addEventListener('click', handleFirstInteraction)
+    window.addEventListener('keydown', handleFirstInteraction)
+
+    return () => {
+      window.removeEventListener('click', handleFirstInteraction)
+      window.removeEventListener('keydown', handleFirstInteraction)
+    }
+  }, [hasInteracted])
 
   return (
     <>
-      {!isLandscape && <RotateDeviceOverlay />}
-      <TwinkleOverlay />
-      <CloudsBackground />
-      <div className='wrapper'>
-        {isLandscape && (
-          <Book width={width} height={height} isMobile={isMobile} />
-        )}
-      </div>
+      <audio autoPlay loop ref={audioRef} preload='auto'>
+        <source
+          src={`${import.meta.env.BASE_URL}assets/audio/1.mp3`}
+          type='audio/mpeg'
+        />
+      </audio>
+      {isMobile ? (
+        <div className='mobile-book'>
+          {pages.map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              alt={`mob${index}`}
+              loading='lazy'
+              style={{ width: '100vw', height: 'max-content' }}
+            />
+          ))}
+        </div>
+      ) : (
+        <>
+          <TwinkleOverlay />
+          <CloudsBackground />
+          <div className='wrapper'>
+            <Book width={width} height={height} isMobile={isMobile} />
+          </div>
+        </>
+      )}
     </>
   )
 }
